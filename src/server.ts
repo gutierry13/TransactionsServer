8920b1a365d9ae3122f14aa961e9893a0258c08f
Alexandre Gutierry <aleguti1000@hotmail.com>
2023-06-12 11:34:49 -0300

    Added env

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import crypto from 'node:crypto'
 const app = fastify()
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
+  return transaction
 })
 app
   .listen({
