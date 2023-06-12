95cdf37caf05acacd828df7bd83a81192a4097ca
Alexandre Gutierry <aleguti1000@hotmail.com>
2023-06-12 20:00:30 -0300

    Created transaction listing and summary

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 #RF
-- [  ] O usuário deve poder criar uma nova transação;
+- [ X ] O usuário deve poder criar uma nova transação;
 - [  ] O usuário deve poder obter um resumo de sua conta;
-- [  ] O usuário deve poder listar todas transações que ja ocorreram;
-- [  ] O usuário deve poder visualizar uma transação única
+- [ X ] O usuário deve poder listar todas transações que ja ocorreram;
+- [ X ] O usuário deve poder visualizar uma transação única
 
 
 #RN
-- [  ] A transação pode ser do tipo crédito que somará a um valor total, ou débito que irá subtrair
+- [ X ] A transação pode ser do tipo crédito que somará a um valor total, ou débito que irá subtrair
 - [  ] Deve ser possível identificar o usuário entre as requisições
 - [  ] O usuário só pode visualizar transações o qual ele criou
 
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,9 +1,43 @@
 import { FastifyInstance } from 'fastify'
-import knex from 'knex'
+import { randomUUID } from 'node:crypto'
+import { z } from 'zod'
+import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transaction = await knex('transactions').select('*')
-    return transaction
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', {
+        as: 'amount',
+      })
+      .first()
+    return { summary }
+  })
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+    return { transactions }
+  })
+  app.get('/:id', async (request) => {
+    const transactionsParams = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = transactionsParams.parse(request.params)
+    const transaction = await knex('transactions').where('id', id).first()
+    return { transaction }
+  })
+  app.post('/', async (request, reply) => {
+    const createTranasctionsBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+    const { title, amount, type } = createTranasctionsBodySchema.parse(
+      request.body,
+    )
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+    reply.status(201).send()
   })
 }
